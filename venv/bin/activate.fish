0bad68874531eb71ba69c5056b3d2dbe44db13b7
john collins <john.collins@edhec.com>
2022-01-31 17:34:52 +0800

    v0.1.1 updated hfdata.ipynb

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/climatemetrics/working/high-frequency-data/venv"
+set -gx VIRTUAL_ENV "/home/climatemetrics/posts/high-frequency-data/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
